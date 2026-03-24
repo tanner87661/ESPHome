78074b61fd69c901850e028e0ef1826a7e8a7b48
Hans R. Tanner <hans@tanner-family.org>
2026-03-24 15:20:55 -0400

    chatgpt versin

--- components/loconet_tcp/loconet_tcp.cpp
+++ components/loconet_tcp/loconet_tcp.cpp
@@ -1,71 +1,62 @@
 #include "loconet_tcp.h"
 #include "esphome/core/log.h"
 
-namespace esphome {
 namespace loconet_tcp {
 
-static const char *const TAG = "loconet_tcp";
+static const char *TAG = "loconet_tcp";
 
-void LoconetTCP::setup() {
-  this->connect_();
+LocoNetTCP::LocoNetTCP(std::string host, uint16_t port)
+    : host_(host), port_(port) {}
+
+void LocoNetTCP::setup() {
+  connect_();
 }
 
-void LoconetTCP::loop() {
+void LocoNetTCP::loop() {
   if (!client_.connected()) {
-    if (millis() - last_connect_attempt_ > 5000) {  // retry every 5 s – keeps connection "permanently on"
-      this->connect_();
-    }
+    ESP_LOGW(TAG, "Disconnected. Reconnecting...");
+    connect_();
     return;
   }
 
   while (client_.available()) {
     char c = client_.read();
     buffer_ += c;
-    if (buffer_.size() >= 2 && buffer_.substr(buffer_.size() - 2) == "\r\n") {
+
+    if (buffer_.size() >= 2 &&
+        buffer_.substr(buffer_.size() - 2) == "\r\n") {
+
       std::string line = buffer_.substr(0, buffer_.size() - 2);
       buffer_.clear();
-      this->process_line_(line);
+
+      ESP_LOGD(TAG, "Received: %s", line.c_str());
+      handle_line_(line);
     }
   }
 }
 
-void LoconetTCP::connect_() {
-  last_connect_attempt_ = millis();
-  ESP_LOGI(TAG, "Connecting to %s:%d ...", host_.c_str(), port_);
-  if (client_.connect(host_.c_str(), port_)) {
-    ESP_LOGI(TAG, "TCP connected (permanent keep-alive)");
-    buffer_.clear();
-  } else {
-    ESP_LOGW(TAG, "Connect failed, will retry");
-  }
+void LocoNetTCP::connect_() {
+  ESP_LOGI(TAG, "Connecting to %s:%d", host_.c_str(), port_);
+  client_.connect(host_.c_str(), port_);
 }
 
-void LoconetTCP::process_line_(const std::string &line) {
-  ESP_LOGV(TAG, "Received: %s", line.c_str());
-
-  if (line.rfind("RECEIVE", 0) == 0 && receive_trigger_) {
-    receive_trigger_->trigger(line);
-  } else if (line.rfind("SENT OK", 0) == 0 && sent_ok_trigger_) {
-    sent_ok_trigger_->trigger(line);
-  }
-}
-
-void LoconetTCP::send(const std::string &data) {
+void LocoNetTCP::send(std::string msg) {
   if (client_.connected()) {
-    client_.print(data);
+    client_.print(msg.c_str());
     client_.print("\r\n");
-    client_.flush();
-    ESP_LOGD(TAG, "Sent: %s", data.c_str());
   } else {
-    ESP_LOGW(TAG, "Cannot send – not connected");
+    ESP_LOGW(TAG, "Send failed, not connected");
   }
 }
 
-void LoconetTCP::dump_config() {
-  ESP_LOGCONFIG(TAG, "LoConet TCP Client:");
-  ESP_LOGCONFIG(TAG, "  Host: %s", host_.c_str());
-  ESP_LOGCONFIG(TAG, "  Port: %d", port_);
+void LocoNetTCP::handle_line_(const std::string &line) {
+  if (line.rfind("RECEIVE", 0) == 0) {
+    if (receive_trigger_ != nullptr)
+      receive_trigger_->trigger(line);
+  } else if (line.rfind("SENT OK", 0) == 0) {
+    if (sent_ok_trigger_ != nullptr)
+      sent_ok_trigger_->trigger(line);
+  }
 }
 
 }  // namespace loconet_tcp
-}  // namespace esphome
--- components/loconet_tcp/loconet_tcp.h
+++ components/loconet_tcp/loconet_tcp.h
@@ -1,40 +1,45 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/core/automation.h"
-#include <WiFiClient.h>
-#include <string>
+#include "esphome.h"
+#include <ESP8266WiFi.h>
 
-namespace esphome {
 namespace loconet_tcp {
 
-class LoconetTCP : public Component {
+class LocoNetTCP;
+
+class ReceiveTrigger : public Trigger<std::string> {
  public:
+  ReceiveTrigger(LocoNetTCP *parent) { parent->set_receive_trigger(this); }
+};
+
+class SentOkTrigger : public Trigger<std::string> {
+ public:
+  SentOkTrigger(LocoNetTCP *parent) { parent->set_sent_ok_trigger(this); }
+};
+
+class LocoNetTCP : public Component {
+ public:
+  LocoNetTCP(std::string host, uint16_t port);
+
   void setup() override;
   void loop() override;
-  void dump_config() override;
 
-  void set_host(const std::string &host) { host_ = host; }
-  void set_port(uint16_t port) { port_ = port; }
+  void send(std::string msg);
 
-  void set_receive_trigger(Trigger<std::string> *trigger) { receive_trigger_ = trigger; }
-  void set_sent_ok_trigger(Trigger<std::string> *trigger) { sent_ok_trigger_ = trigger; }
-
-  // Action you can call from YAML: id(my_loconet).send("COMMAND")
-  void send(const std::string &data);
+  void set_receive_trigger(ReceiveTrigger *t) { receive_trigger_ = t; }
+  void set_sent_ok_trigger(SentOkTrigger *t) { sent_ok_trigger_ = t; }
 
  protected:
+  void connect_();
+  void handle_line_(const std::string &line);
+
   std::string host_;
-  uint16_t port_{1234};
+  uint16_t port_;
   WiFiClient client_;
   std::string buffer_;
-  uint32_t last_connect_attempt_{0};
-  Trigger<std::string> *receive_trigger_{nullptr};
-  Trigger<std::string> *sent_ok_trigger_{nullptr};
 
-  void connect_();
-  void process_line_(const std::string &line);
+  ReceiveTrigger *receive_trigger_{nullptr};
+  SentOkTrigger *sent_ok_trigger_{nullptr};
 };
 
 }  // namespace loconet_tcp
-}  // namespace esphome
